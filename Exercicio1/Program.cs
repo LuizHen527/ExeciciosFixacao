165428ac12ce5d67e05490cc76e7eda7bf22d77a
Luiz <38606125814@info.local>
2023-04-13 10:50:09 -0300

    Terminei o exercicio 2

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,29 @@
+﻿// - Insira seu ano de nacimento:
+// - processar
+// 	- idade = anoAtual - anoNascimento;
+// 	- if(idade => 18)
+// 		- Pode votar
+// 	- else
+// 		- Nao pode votar
+
+Console.WriteLine($"Insira seu ano de nascimento:");
+int anoNascimento = int.Parse(Console.ReadLine());
+
+DateTime dataAtual = DateTime.Now;
+int anoAtual = dataAtual.Year;
+int idade = anoAtual - anoNascimento;
+
+Console.WriteLine(idade);
+Console.WriteLine(anoAtual);
+
+
+
+if(idade < 18)
+{
+    Console.WriteLine($"Nao pode votar");
+}
+
+else
+{
+    Console.WriteLine($"Pode votar");
+}
---
+++ Exercicio2/Program.cs
@@ -0,0 +1,86 @@
+﻿// - Insira o tipo de combustivel (A para alcool e G para gasolina)
+// - Insira a quantidade de combustivel (litros)
+// - Criar metodo Alcool (Retorna o total a pagar)
+// 	- Valores: Quantidade
+// 	- if(Quantidade <= 20)
+// 		- desconto = Quantidade/0.03
+// 		- preco = 4.90*Quantidade
+// 		- return preco - desconto
+// 	- else 
+// 		- desconto = Quantidade/0.05
+// 		- preco = 4.90*Quantidade
+// 		- return preco - desconto
+
+// -Criar metodo Gasolina
+// 	- Valores: Quantidade
+// 	- if(Quantidade <= 20)
+// 		- desconto = Quantidade/0.04
+// 		- preco = 5.30*Quantidade
+// 		- return preco - desconto
+// 	- else 
+// 		- desconto = Quantidade/0.06
+// 		- preco = 5.30*Quantidade
+// 		- return preco - desconto
+
+// - if(tipo == 'A')
+// 	- O total a pagar e R$ {Alcool}
+// - if(tipo == 'G')
+// 	- O total a pagar e R$ {Gasolina}
+
+//Code:
+
+Console.WriteLine($"Insira o tipo de combustivel: ('A' para alcool ou 'G' para gasolina)");
+char tipo = char.Parse(Console.ReadLine().ToUpper());
+
+Console.WriteLine($"Insira a quantidade de combustivel: (litros)");
+float quantidade = float.Parse(Console.ReadLine());
+
+
+static double Alcool(float Quantidade)
+{
+    if(Quantidade <= 20)
+    {
+        double desconto = Quantidade*(4.9*0.03);
+ 		double preco = 4.90*Quantidade;
+ 		return preco - desconto;
+    }
+ 		
+ 	else 
+    {
+        double desconto = Quantidade*(4.9*0.05);
+ 		double preco = 4.90*Quantidade;
+ 		return preco - desconto;
+    }
+ 		
+}
+static double Gasolina(float Quantidade)
+{
+    if(Quantidade <= 20)
+    {
+        double desconto = Quantidade*(5.3*0.04);
+ 		double preco = 5.3*Quantidade;
+ 		return preco - desconto;
+    }
+ 		
+ 	else 
+    {
+        double desconto = Quantidade*(4.9*0.06);
+ 		double preco = 5.3*Quantidade;
+ 		return preco - desconto;
+    }
+ 		
+}
+
+switch(tipo)
+{
+    case 'A':
+    Console.WriteLine($"Total a pagar no alcool: R$ {Math.Round(Alcool(quantidade), 2)}");
+    break;
+
+    case 'G':
+    Console.WriteLine($"Total a pagar na gasolina: R$ {Math.Round(Gasolina(quantidade), 2)}");
+    break;
+
+    default:
+    break;
+}
