9a52e0754ee82a20ec93e0510962c548a3245e9f
LuizHen527 <125361452+LuizHen527@users.noreply.github.com>
2023-04-18 07:54:14 -0300

    exec 5 e 6

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,17 @@
+﻿int y = 1;
+int g = 0;
+
+for (int x = 0; x < 11; x++)
+{
+    while (g < x)
+    {
+        for (; y < 11; y++)
+        {
+            int produto = x*y;
+            Console.WriteLine($"{y} x {x} = {produto}");
+        }
+        y=1;
+        g++;
+    }
+    Console.WriteLine($"");
+}
---
+++ Exercicio6/Program.cs
@@ -0,0 +1,38 @@
+﻿// - Insira seu nome
+// - salvar nome em um array
+// - Pesquise um nome
+// 	- if (pesquisa == nome)
+// 		- achou
+// 	- else 
+// 		- nao achou
+
+string[] nomes = new string[3];
+bool estado = false;
+
+for (var nome = 1; nome < 3; nome++)
+{
+    Console.WriteLine($"Insira um nome:");
+    nomes[nome] = Console.ReadLine();   
+}
+
+do
+{
+    Console.WriteLine($"Pesquise um nome:");
+    string pesquisa = Console.ReadLine();
+    
+    foreach (var item in nomes)
+    {
+        if (item == pesquisa)
+        {
+           Console.WriteLine($"achei");
+            estado = true; 
+            break;
+        }
+    }
+    if (estado == false)
+    {
+        Console.WriteLine($"nao achei");
+    }
+   
+    
+} while (estado == false);
