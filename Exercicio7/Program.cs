01e1f21fa6b0f3e0f2702be9cce092d8e2bf3938
LuizHen527 <125361452+LuizHen527@users.noreply.github.com>
2023-04-17 22:26:13 -0300

    fiz o 7

---
+++ Exercicio7/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] numeros = new int[15];
+for (int i = 0; i < 15; i++)
+{
+    Console.WriteLine("");
+    Console.WriteLine($"Insira o {i + 1}º numero:");
+    numeros[i] = int.Parse(Console.ReadLine());
+
+}
+Console.WriteLine("");
+Array.Reverse(numeros);
+
+foreach (var numero in numeros)
+{
+    Console.WriteLine(numero);
+}
